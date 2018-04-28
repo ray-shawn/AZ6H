c845d358220705d8b8d0c4a4e05d9ab16c49b28a
weixiong <1039796962@qq.com>
2018-04-28 16:25:10 +0800

    Add module stuff for andriod

--- imleagues/src/examples/misc/NativeExample.js
+++ imleagues/src/examples/misc/NativeExample.js
@@ -3,7 +3,11 @@ import {
   View,
   Platform,
   Text,
+  NativeAppEventEmitter,
+  ToastAndroid,
 } from "react-native";
+import RNReactNativeMyLib from 'react-native-myLib';
+
 
 class NativeExample extends React.Component {
   constructor(props) {
@@ -18,10 +22,25 @@ class NativeExample extends React.Component {
     /* eslint-enable */
   }
 
+
+  componentDidMount() {
+    RNReactNativeMyLib.show('Awesome', 10);
+
+    NativeAppEventEmitter.addListener(
+      "SwipeCardEvent",
+      (s) => {
+        ToastAndroid.show(s + " from SwipeCardEvent", 10);
+      }
+    );
+
+    RNReactNativeMyLib.rnCallNative("orignal Demo", (s) => {
+      //ToastAndroid.show(s,10);
+    });
+  }
   render() {
     return (
       <View>
-        <View style={{ alignItems: "center", justifyContent: "center"}}>
+        <View style={{ alignItems: "center", justifyContent: "center" }}>
           <Text>demo!</Text>
         </View>
       </View>
